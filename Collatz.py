1b642e42a66142827dc11aff5b7787b8d5e61757
Doug <ddg625@cs.utexas.edu>
2014-01-20 13:53:34 -0600

    Restored code to previous state

--- Collatz.py
+++ Collatz.py
@@ -15,25 +15,28 @@ cash = [0] * 100000
 # collatz_read
 # ------------
 
-def collatz_read (r) :
+def collatz_read (r, a) :
     """
-    r is a  reader
-    returns an generator that iterates over a sequence of lists of ints of length 2
-    for s in r :
-        l = s.split()
-        b = int(l[0])
-        e = int(l[1])
-        yield [b, e]
-    for s in r :
-        yield map(int, s.split())
-    """
-    return (map(int, s.split()) for s in r)
+reads two ints into a[0] and a[1]
+r is a reader
+a is an array of int
+return true if that succeeds, false otherwise
+"""
+    s = r.readline()
+    if s == "" :
+        return False
+    l = s.split()
+    a[0] = int(l[0])
+    a[1] = int(l[1])
+    assert a[0] > 0
+    assert a[1] > 0
+    return True
 
 # ------------
 # collatz_eval
 # ------------
 
-def collatz_eval ((i, j)) :
+def collatz_eval (i, j) :
     """
 i is the beginning of the range, inclusive
 j is the end of the range, inclusive
@@ -64,9 +67,9 @@ return the max cycle length in the range [i, j]
 
 def collatz_cycle (n) :
     """
-    n is the number for which cycle length is calculated
-    return the cycle length for n
-    """
+n is the number for which cycle length is calculated
+return the cycle length for n
+"""
     global cash
     assert n > 0
     if (n < 100000) and (cash[n] != 0) :
@@ -85,14 +88,15 @@ def collatz_cycle (n) :
 # -------------
 # collatz_print
 # -------------
-def collatz_print (w, (i, j), v) :
+
+def collatz_print (w, i, j, v) :
     """
-    prints the values of i, j, and v
-    w is a writer
-    v is the max cycle length
-    i is the beginning of the range, inclusive
-    j is the end       of the range, inclusive
-    """
+prints the values of i, j, and v
+w is a writer
+i is the beginning of the range, inclusive
+j is the end of the range, inclusive
+v is the max cycle length
+"""
     w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
 
 # -------------
@@ -101,10 +105,11 @@ def collatz_print (w, (i, j), v) :
 
 def collatz_solve (r, w) :
     """
-    read, eval, print loop
-    r is a reader
-    w is a writer
-    """
-    for t in collatz_read(r) :
-        v = collatz_eval(t)
-        collatz_print(w, t, v)
+read, eval, print loop
+r is a reader
+w is a writer
+"""
+    a = [0, 0]
+    while collatz_read(r, a) :
+        v = collatz_eval(a[0], a[1])
+        collatz_print(w, a[0], a[1], v)
