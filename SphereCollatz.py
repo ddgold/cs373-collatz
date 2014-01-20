7fd45c6ba1f978a4859864237176e73b45ca9607
Doug <ddg625@cs.utexas.edu>
2014-01-20 13:13:40 -0600

    Undated ShereCollatz.py

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -13,28 +13,25 @@ cash = [0] * 100000
 # collatz_read
 # ------------
 
-def collatz_read (r, a) :
+def collatz_read (r) :
     """
-reads two ints into a[0] and a[1]
-r is a reader
-a is an array of int
-return true if that succeeds, false otherwise
-"""
-    s = r.readline()
-    if s == "" :
-        return False
-    l = s.split()
-    a[0] = int(l[0])
-    a[1] = int(l[1])
-    assert a[0] > 0
-    assert a[1] > 0
-    return True
+    r is a  reader
+    returns an generator that iterates over a sequence of lists of ints of length 2
+    for s in r :
+        l = s.split()
+        b = int(l[0])
+        e = int(l[1])
+        yield [b, e]
+    for s in r :
+        yield map(int, s.split())
+    """
+    return (map(int, s.split()) for s in r)
 
 # ------------
 # collatz_eval
 # ------------
 
-def collatz_eval (i, j) :
+def collatz_eval ((i, j)) :
     """
 i is the beginning of the range, inclusive
 j is the end of the range, inclusive
@@ -65,9 +62,9 @@ return the max cycle length in the range [i, j]
 
 def collatz_cycle (n) :
     """
-n is the number for which cycle length is calculated
-return the cycle length for n
-"""
+    n is the number for which cycle length is calculated
+    return the cycle length for n
+    """
     global cash
     assert n > 0
     if (n < 100000) and (cash[n] != 0) :
@@ -86,15 +83,14 @@ return the cycle length for n
 # -------------
 # collatz_print
 # -------------
-
-def collatz_print (w, i, j, v) :
+def collatz_print (w, (i, j), v) :
     """
-prints the values of i, j, and v
-w is a writer
-i is the beginning of the range, inclusive
-j is the end of the range, inclusive
-v is the max cycle length
-"""
+    prints the values of i, j, and v
+    w is a writer
+    v is the max cycle length
+    i is the beginning of the range, inclusive
+    j is the end       of the range, inclusive
+    """
     w.write(str(i) + " " + str(j) + " " + str(v) + "\n")
 
 # -------------
@@ -103,14 +99,13 @@ v is the max cycle length
 
 def collatz_solve (r, w) :
     """
-read, eval, print loop
-r is a reader
-w is a writer
-"""
-    a = [0, 0]
-    while collatz_read(r, a) :
-        v = collatz_eval(a[0], a[1])
-        collatz_print(w, a[0], a[1], v)
+    read, eval, print loop
+    r is a reader
+    w is a writer
+    """
+    for t in collatz_read(r) :
+        v = collatz_eval(t)
+        collatz_print(w, t, v)
 
 # ----
 # main
